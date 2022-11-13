b8af7035b6b771d97bea6b089091ea6abb0ff08c
RBarroco <barrocoeb@gmail.com>
2022-11-13 14:21:08 -0500

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -10,9 +10,9 @@ app = FastAPI()
 def pong():
     return {"ping": "pong!"}
 
-# pydantic models
+# pydantic models should we input dict
 class Input(BaseModel):
-    input: str
+    input: dict
 
 class Output(Input):
     forecast: int
@@ -30,3 +30,34 @@ def get_prediction(payload: Input):
         "input": input, 
         "output": converts(output)}
     return response_object
+
+
+#     {'Unnamed: 0': 0,
+#   'Received Packets': 132,
+#   'Received Bytes': 9181,
+#   'Sent Bytes': 6311853,
+#   'Sent Packets': 238,
+#   'Port alive Duration (S)': 46,
+#   'Delta Received Packets': 0,
+#   'Delta Received Bytes': 0,
+#   'Delta Sent Bytes': 280,
+#   'Delta Sent Packets': 2,
+#   'Delta Port alive Duration (S)': 5,
+#   'Connection Point': 1,
+#   'Total Load/Rate': 0,
+#   'Total Load/Latest': 0,
+#   'Active Flow Entries': 9,
+#   'Switch ID_of:0000000000000002': 0,
+#   'Switch ID_of:0000000000000003': 0,
+#   'Switch ID_of:0000000000000004': 0,
+#   'Switch ID_of:0000000000000005': 0,
+#   'Switch ID_of:0000000000000006': 0,
+#   'Switch ID_of:0000000000000007': 0,
+#   'Switch ID_of:0000000000000008': 0,
+#   'Switch ID_of:0000000000000009': 0,
+#   'Switch ID_of:000000000000000a': 0,
+#   'Switch ID_of:000000000000000b': 0,
+#   'Switch ID_of:000000000000000c': 1,
+#   'Port Number_Port#:2': 0,
+#   'Port Number_Port#:3': 0,
+#   'Port Number_Port#:4': 0}
