9f64d3dfed74fb70126bc6312a3d8c98f3b6ce29
RBarroco <barrocoeb@gmail.com>
2022-11-13 20:29:47 -0500

    parsing information

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Query, HTTPException
-import pandas
+import pandas as pd
 from pydantic import BaseModel
+from typing import Dict
 from model import predict
 
 app = FastAPI()
@@ -12,10 +13,10 @@ def pong():
 
 # pydantic models should we input dict
 class Input(BaseModel):
-    input: dict
+    input: str
 
-class Output(Input):
-    forecast: int
+class Output(BaseModel):
+    forecast: str
 
 @app.post("/predict", response_model=Output, status_code=200)
 def get_prediction(payload: Input):
@@ -26,39 +27,16 @@ def get_prediction(payload: Input):
     if not output:
         raise HTTPException(status_code=400, detail="Model not found.")
 
-    response_object = {
-        "input": input, 
-        "output": output
-        }
-    return response_object
+    return output
 
 
-#     {'Unnamed: 0': 0,
-#   'Received Packets': 132,
-#   'Received Bytes': 9181,
-#   'Sent Bytes': 6311853,
-#   'Sent Packets': 238,
-#   'Port alive Duration (S)': 46,
-#   'Delta Received Packets': 0,
-#   'Delta Received Bytes': 0,
-#   'Delta Sent Bytes': 280,
-#   'Delta Sent Packets': 2,
-#   'Delta Port alive Duration (S)': 5,
-#   'Connection Point': 1,
-#   'Total Load/Rate': 0,
-#   'Total Load/Latest': 0,
-#   'Active Flow Entries': 9,
-#   'Switch ID_of:0000000000000002': 0,
-#   'Switch ID_of:0000000000000003': 0,
-#   'Switch ID_of:0000000000000004': 0,
-#   'Switch ID_of:0000000000000005': 0,
-#   'Switch ID_of:0000000000000006': 0,
-#   'Switch ID_of:0000000000000007': 0,
-#   'Switch ID_of:0000000000000008': 0,
-#   'Switch ID_of:0000000000000009': 0,
-#   'Switch ID_of:000000000000000a': 0,
-#   'Switch ID_of:000000000000000b': 0,
-#   'Switch ID_of:000000000000000c': 1,
-#   'Port Number_Port#:2': 0,
-#   'Port Number_Port#:3': 0,
-#   'Port Number_Port#:4': 0}
+# @app.post("/predict", response_model=Output, status_code=200)
+# def get_prediction(payload: Input):
+#     input = pd.DataFrame.read_json(payload.input)
+
+#     output = predict(input)
+
+#     if not output:
+#         raise HTTPException(status_code=400, detail="Model not found.")
+
+#     return output
--- src/model.py
+++ src/model.py
@@ -3,39 +3,38 @@ import pandas as pd
 from pathlib import Path
 
 
-BASE_DIR="finalized_model.sav"
+BASE_DIR="../model-info/finalized_model.joblib"
 # import will be a row for example from a dataframe
-# value = pd.read_csv("../data/batch_load/preprocessed_data/X_batch_v2.csv")
-input = {
-  'Unnamed: 0': 0,
-  'Received Packets': 132,
-  'Received Bytes': 9181,
-  'Sent Bytes': 6311853,
-  'Sent Packets': 238,
-  'Port alive Duration (S)': 46,
-  'Delta Received Packets': 0,
-  'Delta Received Bytes': 0,
-  'Delta Sent Bytes': 280,
-  'Delta Sent Packets': 2,
-  'Delta Port alive Duration (S)': 5,
-  'Connection Point': 1,
-  'Total Load/Rate': 0,
-  'Total Load/Latest': 0,
-  'Active Flow Entries': 9,
-  'Switch ID_of:0000000000000002': 0,
-  'Switch ID_of:0000000000000003': 0,
-  'Switch ID_of:0000000000000004': 0,
-  'Switch ID_of:0000000000000005': 0,
-  'Switch ID_of:0000000000000006': 0,
-  'Switch ID_of:0000000000000007': 0,
-  'Switch ID_of:0000000000000008': 0,
-  'Switch ID_of:0000000000000009': 0,
-  'Switch ID_of:000000000000000a': 0,
-  'Switch ID_of:000000000000000b': 0,
-  'Switch ID_of:000000000000000c': 1,
-  'Port Number_Port#:2': 0,
-  'Port Number_Port#:3': 0,
-  'Port Number_Port#:4': 0}
+
+# input = 
+# {'Received Packets': {24789: 4188},
+#  'Received Bytes': {24789: 75785402},
+#  'Sent Bytes': {24789: 137303965},
+#  'Sent Packets': {24789: 417351},
+#  'Port alive Duration (S)': {24789: 2027},
+#  'Delta Received Packets': {24789: 0},
+#  'Delta Received Bytes': {24789: 0},
+#  'Delta Sent Bytes': {24789: 556},
+#  'Delta Sent Packets': {24789: 4},
+#  'Delta Port alive Duration (S)': {24789: 5},
+#  'Connection Point': {24789: 2},
+#  'Total Load/Rate': {24789: 0},
+#  'Total Load/Latest': {24789: 0},
+#  'Active Flow Entries': {24789: 4},
+#  'Switch ID_of:0000000000000002': {24789: 0},
+#  'Switch ID_of:0000000000000003': {24789: 1},
+#  'Switch ID_of:0000000000000004': {24789: 0},
+#  'Switch ID_of:0000000000000005': {24789: 0},
+#  'Switch ID_of:0000000000000006': {24789: 0},
+#  'Switch ID_of:0000000000000007': {24789: 0},
+#  'Switch ID_of:0000000000000008': {24789: 0},
+#  'Switch ID_of:0000000000000009': {24789: 0},
+#  'Switch ID_of:000000000000000a': {24789: 0},
+#  'Switch ID_of:000000000000000b': {24789: 0},
+#  'Switch ID_of:000000000000000c': {24789: 0},
+#  'Port Number_Port#:2': {24789: 1},
+#  'Port Number_Port#:3': {24789: 0},
+#  'Port Number_Port#:4': {24789: 0}}
 
 
 def predict(input):
@@ -43,49 +42,26 @@ def predict(input):
     if not model_file.exists():
         return False
 
-    input = pd.DataFrame.from_dict(input, orient ='index')
-    input = input.T
-    
+    input = pd.DataFrame.from_json(input)
     loaded_model = joblib.load(model_file)
-    print(1)
-
-    output = loaded_model.predict(input)
-    print(1)
+    output = loaded_model.predict(input)[0]
+    
+    
 #    model.plot(forecast).savefig(f"{ticker}_plot.png")
 #    model.plot_components(forecast).savefig(f"{ticker}_plot_components.png")
 
-    # converts = {
-    #             0: "Blackhole",
-    #             1: "Diversion",
-    #             2: "Normal",
-    #             3: "Overflow",
-    #             4: "PortScan",
-    #             5: "TCP-SYN" 
-    #             }
+    converts = {
+                0: "Blackhole",
+                1: "Diversion",
+                2: "Normal",
+                3: "Overflow",
+                4: "PortScan",
+                5: "TCP-SYN" 
+                }
 
-    return output
+    return converts[output]
 
-output = predict(input)
-print(output)
-# def converts(output):
-#     if output == 0:
-#         return "Blackhole"
-#     elif output == 1:
-#         return "Diversion"
-#     elif output == 2:
-#         return "Normal"
-#     elif output == 3:
-#         return "Overflow"
-#     elif output == 4:
-#         return "PortScan"
-#     elif output == 5:
-#         return "TCP-SYN"
-
-# 
+# output = predict(input)
+# print(output)
 
 
-# tasks:
-# 1) Check best way to save a xgboost model
-# 2) make that model.py file is working well
-# 3) Requirements.txt
-# 4) 
